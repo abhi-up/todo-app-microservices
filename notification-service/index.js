ad880258dda4cd4006a5d8f14672489825ce0b40
Abhishek <meabhishek1512@gmail.com>
2025-12-29 22:00:59 +0530

    integrate RabbitMQ message consumer for notification service

--- notification-service/index.js
+++ notification-service/index.js
@@ -1,60 +1,24 @@
-// Import required packages
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
+const amqp = require("amqplib")
 
-// Initialize Express app
-const app = express()
-const port = 3001
+let channel, connection
 
-// Parse incoming JSON requests
-app.use(bodyParser.json())
-
-// ❌ Local MongoDB (used without Docker)
-// mongoose
-//   .connect("mongodb://localhost:27017/users")
-
-// ✅ MongoDB connection using Docker service name
-mongoose
-  .connect("mongodb://mongo:27017/users")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB Connection error:", err))
-
-// User schema definition
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-})
-
-// User model
-const User = mongoose.model("User", UserSchema)
-
-// Get all users
-app.get("/users", async (req, res) => {
-  const users = await User.find()
-  res.json(users)
-})
-
-// Create a new user
-app.post("/users", async (req, res) => {
-  const { name, email } = req.body
-
+async function start() {
   try {
-    const user = new User({ name, email })
-    await user.save()
-    res.status(201).json(user)
+    connection = await amqp.connect("amqp://rabbitmq")
+    channel = await connection.createChannel()
+
+    await channel.assertQueue("task_created")
+    console.log("Notification Service is listening to messages.")
+
+    channel.consume("task_created", (msg) => {
+      const taskData = JSON.parse(msg.content.toString())
+      console.log("Notification: NEW TASK: ", taskData.title)
+      console.log("Notification: NEW TASK: ", taskData)
+      channel.ack(msg)
+    })
   } catch (error) {
-    console.error("Error saving user:", error)
-    res.status(500).json({ error: "Internal Server Error" })
+    console.error("RabbitMQ Connection Error: ", error)
   }
-})
+}
 
-// Health check route
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+start()
